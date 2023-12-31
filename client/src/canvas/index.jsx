4f8b47d035dda7eff99cd9e4cd8804983d03c18f
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2023-12-31 20:26:29 +0530

    working on 3d canvas

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,4 +1,10 @@
-const Canvas = () => {
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei'
+import Shirt from './Shirt'
+import CameraRig from './CameraRig'
+import Backdrop from './Backdrop'
+
+const CanvasModel = () => {
     return (
         <div>
             Canvas
@@ -6,4 +12,4 @@ const Canvas = () => {
     )
 }
 
-export default Canvas
+export default CanvasModel
