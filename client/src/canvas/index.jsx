78198f2204a2d3fba26263d309804774e28f6a3b
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2023-12-31 21:58:43 +0530

    backdrop done

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -10,6 +10,7 @@ const CanvasModel = () => {
             shadows
             camera={{ position: [0, 0, 0], fov: 25 }} // fov is field of view
             gl={{ preserveDrawingBuffer: true }}
+            className="w-full max-w-full h-full transition-all ease-in"
         >
             <ambientLight intensity={0.5} />
             <Environment preset="city" />
