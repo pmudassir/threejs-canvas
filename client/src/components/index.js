292531a98e6077383689d5af7cf6a2e1929c81d0
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2023-12-31 15:02:47 +0530

    working on customiser

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,7 @@
 import CustomButton from "./CustomButton";
+import Tab from "./Tab";
+import FilePicker from "./FilePicker";
+import ColorPicker from "./ColorPicker";
+import AIPicker from "./AIPicker";
 
-export { CustomButton };
+export { CustomButton, Tab, FilePicker, ColorPicker, AIPicker };
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,3 +1,14 @@
+import { useState, useEffect } from "react"
+import { AnimatePresence, motion } from "framer-motion"
+import { useSnapshot } from "valtio"
+import config from "../config/config"
+import state from "../store"
+import { download } from "../assets"
+import { downloadCanvasToImage, reader } from "../config/helpers"
+import { FilterTabs, EditorTabs, DecalTypes } from "../config/constants"
+import { fadeAnimation, slideAnimation } from "../config/motion"
+import { AIPicker, ColorPicker, FilePicker, Tab, CustomButton } from "../components"
+
 const Customizer = () => {
     return (
         <div>
