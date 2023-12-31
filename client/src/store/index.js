f5b15c03fe040017dcc3a5df43d7063bf4b7b998
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2023-12-31 21:32:40 +0530

    camera done

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,7 +3,7 @@ import { proxy } from "valtio";
 const state = proxy({
     intro: true,
     color: '#EFBD48',
-    isLogoTexture: true,
+    isLogoTexture: false, // i have set this false to avoid anisotropy err
     isFullTexture: false,
     logoDecal: './threejs.png',
     fullDecal: './threejs.png',
