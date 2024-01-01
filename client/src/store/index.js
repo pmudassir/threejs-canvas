f02f4cd27072f65dbb01a64ac00d8c20c0f9c967
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2024-01-01 16:18:36 +0530

    file picker done

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,7 +3,7 @@ import { proxy } from "valtio";
 const state = proxy({
     intro: true,
     color: '#EFBD48',
-    isLogoTexture: false, // i have set this false to avoid anisotropy err
+    isLogoTexture: true, // i have set this false to avoid anisotropy err
     isFullTexture: false,
     logoDecal: './threejs.png',
     fullDecal: './threejs.png',
