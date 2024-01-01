831c8a82dc344a32bec76c98b16be7621892fb65
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2024-01-01 14:18:23 +0530

    color picket done

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { SketchPicker } from 'react-color'
+import { useSnapshot } from 'valtio'
+import state from '../store'
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state)
+
   return (
-    <div>ColorPicker</div>
+    <div className='absolute left-full ml-3'>
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        // presetColors={[
+        //   //we can add preset colors as we need eg:
+        //   '#000000', '#ffffff', '#ff0000', '#00ff00', '#0000ff', '#ffff00',
+        // ]}
+        onChange={(color) => state.color = color.hex}
+      />
+    </div>
   )
 }
 
